38dee2fdf7f12f027c7aed179ee2ade1f4ce2f88
christa-baccas <christa.baccas@gmail.com>
2021-11-02 09:43:21 -0500

    added a render to the users to be displayed on the homepage

--- controller/home-routes.js
+++ controller/home-routes.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
 const { User, Chore } = require('../model');
 
-//GET route to display all username of the members in db
+//GET route to display all usernames of the members in db
 router.get('/', async (req,res) => {
     try {
         const userData = await User.findAll({
@@ -10,6 +10,15 @@ router.get('/', async (req,res) => {
               ],
         });
         res.status(200).json(userData);
+
+    // Serialize data so the template can read it
+    const users = userData.map((user) => user.get({ plain: true }));
+
+    // Pass serialized data and session flag into template
+    res.render('homepage', { 
+        users, 
+    // logged_in: req.session.logged_in 
+    });
     } catch (err) {
         res.status(500).json(err);
     }
