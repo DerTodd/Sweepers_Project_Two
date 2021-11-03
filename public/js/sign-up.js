f297bc9861d7ec6faab948831522934042eb12b5
christa-baccas <christa.baccas@gmail.com>
2021-11-02 20:33:39 -0500

    validated login and signup functions

--- public/js/sign-up.js
+++ public/js/sign-up.js
@@ -10,7 +10,7 @@ const signupFormHandler = async (event) => {
     console.log(password);
 
     if (username && email && password) {
-      const response = await fetch('/api/users/login', {
+      const response = await fetch('/api/users/signup', {
         method: 'POST',
         body: JSON.stringify({ username, email, password }),
         headers: { 'Content-Type': 'application/json' },
