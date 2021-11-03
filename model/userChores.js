5ec0b5c46eb33f598def1411d22b05dad831488c
JAMATX38 <JAIMEMTX38@GMAIL.COM>
2021-11-02 21:04:48 -0500

    userchore modal

---
+++ model/userChores.js
@@ -0,0 +1,42 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class UserChore extends Model { }
+
+UserChore.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        user_id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            references: 'user',
+            key: 'id',
+
+
+        },
+        assign_chore: {
+            type: DataTypes.STRING,
+            allowNull: true,
+        },
+        chore_id: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: 'chore',
+                key: 'id',
+            }
+        },
+    },
+    {
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'userChore'
+    }
+);
+
+module.exports = UserChore;
