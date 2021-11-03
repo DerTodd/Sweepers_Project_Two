e0bd6f40edac11a906e8ca70b454bb842d8b9c60
JAMATX38 <JAIMEMTX38@GMAIL.COM>
2021-11-02 21:34:15 -0500

    update userchores rename

--- model/index.js
+++ model/index.js
@@ -1,6 +1,6 @@
 const User = require('./User');
 const Chore = require('./Chore');
-const UserChore = require('./userChores');
+const UserChore = require('./UserChores');
 
 
 User.hasMany(UserChore,{
