22fd17b2e64c97ee040326609784c026fdeadf94
christa-baccas <christa.baccas@gmail.com>
2021-11-04 10:24:55 -0500

    merge conflict

--- model/UserChores.js
+++ model/UserChores.js
@@ -13,18 +13,11 @@ UserChore.init(
        user_id: {
             type: DataTypes.INTEGER,
             allowNull: false,
-<<<<<<< HEAD
 
             references: {
                 model: 'user',
                 key: 'id',
-            },
-=======
-            references: {
-                model: 'user',
-                key: 'id',
             }
->>>>>>> 9c058ed0bae52bb92657f72c1f9456bbbe121c3a
         },
         assign_chore: {
             type: DataTypes.STRING,
